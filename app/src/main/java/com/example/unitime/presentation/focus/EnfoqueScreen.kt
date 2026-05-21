66312a28f197434cf64bd14de0780ead365f8aa9
PabloArn <arnaudpablo53@gmail.com>
2026-05-21 01:30:14 -0600

    SENSOR DE ENFOQUE V2

--- app/src/main/java/com/example/unitime/presentation/focus/EnfoqueScreen.kt
+++ app/src/main/java/com/example/unitime/presentation/focus/EnfoqueScreen.kt
@@ -9,6 +9,7 @@ import android.media.RingtoneManager
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
